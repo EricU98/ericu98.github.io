ad499eea22b9182e73f9cbd650f70c28c1b8b428
Eric <108857582+EricU98@users.noreply.github.com>
2023-02-13 15:56:21 +0100

    Updated Files

--- WebItemList/app.js
+++ WebItemList/app.js
@@ -1,3 +1,5 @@
+//TODO Get current List + Load current List
+
 $(document).ready(function () {
    //ANCHOR - Get Items
    reload();
@@ -109,24 +111,11 @@ function addNewItem(input) {
          }
       } else {
          showErrorMessage("1001");
+         //showErrorAlert();
       }
 
       reload();
       $(input).val("");
-   });
-}
-
-//Shows error message based on the error code passed in
-function showErrorMessage(errorCode) {
-   $.getJSON("error-messages.json", function (data) {
-      var errorMessage = "Error message not found.";
-      $.each(data.errorMessages, function (i, item) {
-         if (item.errorCode === errorCode) {
-            errorMessage = item.errorMessage;
-            return false;
-         }
-      });
-      console.log(errorMessage);
    });
 }
 
@@ -165,7 +154,109 @@ $(document).ready(function () {
 
          $("#input-list-name").val("");
       } else {
-         console.log("Der Name der Liste darf nicht leer sein!");
+         showErrorAlertModal();
+         //console.log("Der Name der Liste darf nicht leer sein!");
       }
    });
+});
+
+//Show Error Alert
+function showErrorAlertModal() {
+   $(document).ready(function () {
+      $("#modal-error-alert").show("shake", {}, 400, function () {
+         setTimeout(function () {
+            $("#modal-error-alert").fadeOut(100);
+         }, 3000);
+      });
+   });
+}
+
+function showErrorAlert(message) {
+   $(document).ready(function () {
+      $("#error-message").text(message);
+      $("#error-alert").show("shake", {}, 400, function () {
+         setTimeout(function () {
+            $("#error-alert").fadeOut(100);
+         }, 3000);
+      });
+   });
+}
+
+//Shows error message based on the error code passed in
+function showErrorMessage(errorCode) {
+   $.getJSON("error-messages.json", function (data) {
+      var errorMessage = "Error message not found.";
+      $.each(data.errorMessages, function (i, item) {
+         if (item.errorCode === errorCode) {
+            errorMessage = item.errorMessage;
+            return false;
+         }
+      });
+      showErrorAlert(errorMessage);
+   });
+}
+
+$(document).ready(function () {
+   $("#btn-settings-mobile").click(function () {
+      $("#settings-mobile").show("fold", function () {
+         setTimeout(function () {
+            $("#settings-mobile").hide("fold");
+         }, 10000);
+      });
+   });
+});
+
+$(document).ready(function () {
+   $("#btn-settings-desktop").click(function () {
+      $("#settings-desktop").show(function () {
+         setTimeout(function () {
+            $("#settings-desktop").hide("drop");
+         }, 3000);
+      });
+   });
+});
+
+//Download List as Json File
+//TODO Add Custom File Name
+//TODO Add List Selection
+$(document).ready(function () {
+   $("#btn-download").click(function () {
+      var data = localStorage.getItem("items");
+      var array = JSON.parse(data);
+
+      var json = JSON.stringify(array);
+      var blob = new Blob([json], { type: "application/json" });
+      var url = URL.createObjectURL(blob);
+
+      var a = document.createElement("a");
+      a.download = "array.json";
+      a.href = url;
+      a.click();
+   });
+});
+
+//Upload Json File
+//TODO Add Custom List Name Input
+//TODO Set added List to currentList + refresh if needed
+$(document).ready(function () {
+   $("#btn-upload").click(function () {
+      $("#fileInput").trigger("click");
+   });
+
+   $("#fileInput").change(function () {
+      var file = this.files[0];
+
+      var reader = new FileReader();
+      reader.readAsText(file);
+
+      reader.onload = function () {
+         var json = reader.result;
+         var array = JSON.parse(json);
+         localStorage.setItem("items", JSON.stringify(array));
+      };
+   });
+});
+
+$(function () {
+   $("#itemList").sortable();
 });
